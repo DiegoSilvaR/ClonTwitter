9fac83022da4e8ceba45a4c556e192058e75e395
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-01 20:38:21 -0400

    se da estilo a otras vistasç

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,7 +31,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
+        format.html { redirect_to tweet_url(@tweet), notice: "Tweet creado correctamente" }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -44,7 +44,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." }
+        format.html { redirect_to tweet_url(@tweet), notice: "El Tweet se actualizo correctamente" }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,7 +58,7 @@ class TweetsController < ApplicationController
     @tweet.destroy
 
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.html { redirect_to tweets_url, notice: "El Tweet fue eliminado correctamente" }
       format.json { head :no_content }
     end
   end
